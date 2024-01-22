f797a68859c7b8a097fff6cb8b469b98cff6580d
Austin Pearson <124529429+Apearson14@users.noreply.github.com>
2024-01-22 09:50:21 -0500

    Update MainView.jsx

--- src/components/MainView/MainView.jsx
+++ src/components/MainView/MainView.jsx
@@ -1,56 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../MovieCard/MovieCard";
 import { MovieView } from "../MovieView/MovieView";
 
+export const MainView = () => {
+  const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
-    export const MainView = () => {
-        const [ movie, setMovie] = useState([
-            {
-                id: 1,
-                title: "Step Brothers",
-                image:
-                "m.media-amazon.com/images/M/MV5BODViZDg3ZjYtMzhiYS00YTVkLTk4MzktYWUxMTlkYjc1NjdlXkEyXkFqcGdeQXVyMTMxODk2OTU@.V1_FMjpg_UX1079.jpg",
-                director: "Adam McKay"
-            },
-            {
-                id: 2,
-                title: "300",
-                image:
-                "https://m.media-amazon.com/images/I/811XNNbIaML._AC_SX679_.jpg",
-                director: "Zack Snyder"
-            },
-            {
-                id: 3,
-                title: "Grown ups",
-                image: 
-                "https://m.media-amazon.com/images/I/71LYatfiX-L._AC_SX679_.jpg",
-                director: "Dennis Dungan"
-            },
-        ]);
-        const [selectedMovie, setSelectedMovie] = useState(null);
+  useEffect(() => {
+    // Fetch data from the provided link
+    fetch("https://austins-movies-98c87d76c471.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => setMovies(data))
+      .catch((error) => console.error("Error fetching movies:", error));
+  }, []);
 
-        if (selectedMovie) {
-          return (
-            <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
-          );
-        }
-      
-        if (movie.length === 0) {
-          return <div>The list is empty!</div>;
-        }
-      
-        return (
-          <div>
-            {movie.map((movie) => (
-              <MovieCard
-                key={movie.id}
-                movie={movie}
-                onMovieClick={(newSelectedMovie) => {
-                  setSelectedMovie(newSelectedMovie);
-                }}
-              />
-            ))}
-          </div>
-        );
-      };
-      
+  if (selectedMovie) {
+    return (
+      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
+    );
+  }
+
+  if (movies.length === 0) {
+    return <div>The list is empty!</div>;
+  }
+
+  return (
+    <div>
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          movie={movie}
+          onMovieClick={(newSelectedMovie) => {
+            setSelectedMovie(newSelectedMovie);
+          }}
+        />
+      ))}
+    </div>
+  );
+};
