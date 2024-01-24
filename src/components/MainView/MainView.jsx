4dbe6039b5c70f7069ac66aafe96a601dd20d4d3
Austin Pearson <124529429+Apearson14@users.noreply.github.com>
2024-01-24 18:29:10 -0500

    Update MainView.jsx

--- src/components/MainView/MainView.jsx
+++ src/components/MainView/MainView.jsx
@@ -1,41 +1,62 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { MovieCard } from "../MovieCard/MovieCard";
 import { MovieView } from "../MovieView/MovieView";
+import { LoginView } from "../login-view/login-view";
 
 export const MainView = () => {
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
   const [movies, setMovies] = useState([]);
   const [selectedMovie, setSelectedMovie] = useState(null);
 
   useEffect(() => {
+    if (!token) {
+      return;
+    }
+
     // Fetch data from the provided link
-    fetch("https://austins-movies-98c87d76c471.herokuapp.com/movies")
+    fetch("https://austins-movies-98c87d76c471.herokuapp.com/movies", {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    })
       .then((response) => response.json())
       .then((data) => setMovies(data))
       .catch((error) => console.error("Error fetching movies:", error));
-  }, []);
-
-  if (selectedMovie) {
-    return (
-      <MovieView movie={selectedMovie} onBackClick={() => setSelectedMovie(null)} />
-    );
-  }
-
-  if (movies.length === 0) {
-    return <div>The list is empty!</div>;
-  }
+  }, [token]);
 
   return (
-    <div>
-      {movies.map((movie) => (
-        <MovieCard
-          key={movie.id}
-          movie={movie}
-          onMovieClick={(newSelectedMovie) => {
-            setSelectedMovie(newSelectedMovie);
+    <>
+      {user && (
+        <button
+          onClick={() => {
+            setUser(null);
           }}
+        >
+          Logout
+        </button>
+      )}
+
+      {selectedMovie ? (
+        <MovieView
+          movie={selectedMovie}
+          onBackClick={() => setSelectedMovie(null)}
         />
-      ))}
-    </div>
+      ) : movies.length === 0 ? (
+        <div>The list is empty!</div>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.id}
+              movie={movie}
+              onMovieClick={(newSelectedMovie) => {
+                setSelectedMovie(newSelectedMovie);
+              }}
+            />
+          ))}
+        </div>
+      )}
+    </>
   );
 };
-
