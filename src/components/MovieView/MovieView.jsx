9246e468c81e8bc6d37687380f82c5945c2d53a7
Austin Pearson <124529429+Apearson14@users.noreply.github.com>
2024-01-29 19:01:47 -0500

    Adding Grid to the Movie View

--- src/components/MovieView/MovieView.jsx
+++ src/components/MovieView/MovieView.jsx
@@ -1,18 +1,29 @@
+import React from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+
 export const MovieView = ({ movie, onBackClick }) => {
-    return (
-      <div>
-        <div>
-          <img src={movie.image} />
-        </div>
-        <div>
-          <span>Title: </span>
-          <span>{movie.title}</span>
-        </div>
-        <div>
-          <span>Director: </span>
-          <span>{movie.director}</span>
-        </div>
-        <button onClick={onBackClick}>Back</button>
-      </div>
-    );
-  };
+  return (
+    <Container>
+      <Row>
+        <Col xs={12} md={6}>
+          <img src={movie.image} alt={movie.title} className="img-fluid" />
+        </Col>
+        <Col xs={12} md={6}>
+          <div>
+            <h3>Title:</h3>
+            <p>{movie.title}</p>
+          </div>
+          <div>
+            <h3>Director:</h3>
+            <p>{movie.director}</p>
+          </div>
+          <div>
+            <h3>Description:</h3>
+            <p>{movie.description}</p>
+          </div>  
+          <Button onClick={onBackClick}>Back</Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
