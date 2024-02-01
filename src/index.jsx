cfd4874c8c0b152a3def9f65870d0a912777177f
Austin Pearson <124529429+Apearson14@users.noreply.github.com>
2024-02-01 17:10:50 -0500

    Small updates to comments

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import Container from "react-bootstrap/Container";
 // Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
-// Main component (will eventually use all the others)
+
     const MyFlixApplication = () => {
         return(
         <Container>
@@ -15,9 +15,9 @@ import "./index.scss";
         );
       };
 
-// Finds the root of your app
+
 const container = document.querySelector("#root");
 const root = createRoot(container);
 
-// Tells React to render your app in the root DOM element
+
 root.render(<MyFlixApplication />);
