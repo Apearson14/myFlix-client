103d042023b57a8e0053593031ba462c37671d72
Austin Pearson <124529429+Apearson14@users.noreply.github.com>
2024-02-01 17:03:15 -0500

    Adding Favorite Button to Movie Card

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,33 +1,35 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Card from "react-bootstrap/Card";
-import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 
-export const MovieCard = ({ movie, onMovieClick }) => {
+export const MovieCard = ({ movie, onMovieClick, onToggleFavorite }) => {
+  const handleFavoriteClick = () => {
+    // Call the onToggleFavorite function and pass the movie as an argument
+    onToggleFavorite(movie);
+  };
+
   return (
-    <Card
-      style={{ cursor: "pointer" }}
-      onClick={onMovieClick}
-    >
-      <Link to={`/movies/${movie.id}`}>
-        <Card.Body>
-          <Card.Title>{movie.title}</Card.Title>
-          <Card.Img src={movie.image} />
-          <Card.Text>{movie.description}</Card.Text>
-          <Card.Text>{movie.genre.name}</Card.Text>
-          <Card.Text>{movie.director.name}</Card.Text>
-        </Card.Body>
-      </Link>
+    <Card style={{ cursor: "pointer" }}>
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Img src={movie.image} />
+        <Card.Text>{movie.description}</Card.Text>
+        <Card.Text>{movie.genre.name}</Card.Text>
+        <Card.Text>{movie.director.name}</Card.Text>
+
+        <Button onClick={handleFavoriteClick}>Favorite</Button>
+      </Card.Body>
     </Card>
   );
 };
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    id: PropTypes.string.isRequired,
     title: PropTypes.string,
     image: PropTypes.string,
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired,
+  onToggleFavorite: PropTypes.func.isRequired,
 };
 
