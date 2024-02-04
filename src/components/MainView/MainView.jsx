654efa120506fedd0ec038012ea1c931e04d3ec4
Austin Pearson <124529429+Apearson14@users.noreply.github.com>
2024-02-04 11:42:28 -0500

    Update MainView.jsx

--- src/components/MainView/MainView.jsx
+++ src/components/MainView/MainView.jsx
@@ -71,59 +71,57 @@ export const MainView = () => {
         </Navbar>
 
         <Routes>
-          <Route
-            path="/login"
-            element={
-              <>
-                {user ? (
-                  <Navigate to="/" replace />
-                ) : (
-                  <LoginView onLoggedIn={(user, token) => {
-                    setUser(user);
-                    setToken(token);
-                  }} />
-                )}
-              </>
-            }
-          />
-          <Route
-            path="/signup"
-            element={<SignupView />}
-          />
-          <Route
-            path="/logout"
-            element={<Navigate to="/" replace />}
-          />
-          <Route
-            path="/profile"
-            element={<ProfileView movies={movies} onUpdateUser={handleUpdateUser} onDeregister={handleDeregister} onToggleFavorite={handleToggleFavorite} />}
-          />
-          <Route
-            path="/movies/:movieId"
-            element={<MovieView movies={movies} onToggleFavorite={handleToggleFavorite} />}
-          />
-          <Route
-            path="/"
-            element={
-              movies.length === 0 ? (
-                <Row>
-                  <Col>
-                    <div>The list is empty!</div>
-                  </Col>
-                </Row>
-              ) : (
-                <Row>
-                  {movies.map((movie) => (
-                    <Col key={movie.id} xs={12} sm={6} md={4} lg={3}>
-                      {/* Pass onToggleFavorite to MovieCard */}
-                      <MovieCard movie={movie} onToggleFavorite={handleToggleFavorite} />
-                    </Col>
-                  ))}
-                </Row>
-              )
-            }
-          />
-        </Routes>
+  <Route
+    path="/login"
+    element={
+      user ? (
+        <Navigate to="/" replace />
+      ) : (
+        <LoginView onLoggedIn={(user, token) => {
+          setUser(user);
+          setToken(token);
+        }} />
+      )
+    }
+  />
+  <Route
+    path="/signup"
+    element={<SignupView />}
+  />
+  <Route
+    path="/logout"
+    element={<Navigate to="/" replace />}
+  />
+  <Route
+    path="/profile"
+    element={<ProfileView movies={movies} onUpdateUser={handleUpdateUser} onDeregister={handleDeregister} onToggleFavorite={handleToggleFavorite} />}
+  />
+  <Route
+    path="/movies/:movieId"
+    element={<MovieView movies={movies} onToggleFavorite={handleToggleFavorite} />}
+  />
+  <Route
+    path="/"
+    element={
+      movies.length === 0 ? (
+        <Row>
+          <Col>
+            <div>The list is empty!</div>
+          </Col>
+        </Row>
+      ) : (
+        <Row>
+          {movies.map((movie) => (
+            <Col key={movie.id} xs={12} sm={6} md={4} lg={3}>
+              
+              <MovieCard movie={movie} onToggleFavorite={handleToggleFavorite} />
+            </Col>
+          ))}
+        </Row>
+      )
+    }
+  />
+</Routes>
       </Container>
     </Router>
   );
