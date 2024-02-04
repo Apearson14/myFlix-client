f1da59f4f4072a08dd4b0bb6249305ad7d4b9e01
Austin Pearson <124529429+Apearson14@users.noreply.github.com>
2024-02-04 11:46:44 -0500

    Update MainView.jsx

--- src/components/MainView/MainView.jsx
+++ src/components/MainView/MainView.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Container, Row, Col, Navbar, Nav, Button } from "react-bootstrap";
+import { Container, Row, Col, Navbar, Nav } from "react-bootstrap";
 import { MovieCard } from "../MovieCard/MovieCard";
 import { MovieView } from "../MovieView/MovieView";
 import { LoginView } from "../login-view/login-view";
@@ -8,8 +8,11 @@ import { ProfileView } from "../profile-view/profile-view";
 import { BrowserRouter as Router, Routes, Route, Link, Navigate } from "react-router-dom";
 
 export const MainView = () => {
+  
+  const storedToken = localStorage.getItem("token");
+  const [token, setToken] = useState(storedToken || null);
+
   const [user, setUser] = useState(null);
-  const [token, setToken] = useState(null);
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -17,7 +20,7 @@ export const MainView = () => {
       return;
     }
 
-    // Fetch data from the provided link
+    
     fetch("https://austins-movies-98c87d76c471.herokuapp.com/movies", {
       headers: {
         Authorization: `Bearer ${token}`
@@ -28,21 +31,20 @@ export const MainView = () => {
       .catch((error) => console.error("Error fetching movies:", error));
   }, [token]);
 
-  // Define the function to be passed to ProfileView for updating user information
+  
   const handleUpdateUser = (updatedUser) => {
     setUser(updatedUser);
   };
 
-  // Define the function to be passed to ProfileView for deregistering the user
+  
   const handleDeregister = () => {
     setUser(null);
     setToken(null);
   };
 
-  // Define the function to toggle favorite status
+ 
   const handleToggleFavorite = (selectedMovie) => {
-    // Perform the logic to toggle the favorite status of the movie
-    // This could involve updating the user's data on the server or locally
+    
     console.log(`Toggling favorite for movie: ${selectedMovie.title}`);
   };
 
@@ -71,57 +73,58 @@ export const MainView = () => {
         </Navbar>
 
         <Routes>
-  <Route
-    path="/login"
-    element={
-      user ? (
-        <Navigate to="/" replace />
-      ) : (
-        <LoginView onLoggedIn={(user, token) => {
-          setUser(user);
-          setToken(token);
-        }} />
-      )
-    }
-  />
-  <Route
-    path="/signup"
-    element={<SignupView />}
-  />
-  <Route
-    path="/logout"
-    element={<Navigate to="/" replace />}
-  />
-  <Route
-    path="/profile"
-    element={<ProfileView movies={movies} onUpdateUser={handleUpdateUser} onDeregister={handleDeregister} onToggleFavorite={handleToggleFavorite} />}
-  />
-  <Route
-    path="/movies/:movieId"
-    element={<MovieView movies={movies} onToggleFavorite={handleToggleFavorite} />}
-  />
-  <Route
-    path="/"
-    element={
-      movies.length === 0 ? (
-        <Row>
-          <Col>
-            <div>The list is empty!</div>
-          </Col>
-        </Row>
-      ) : (
-        <Row>
-          {movies.map((movie) => (
-            <Col key={movie.id} xs={12} sm={6} md={4} lg={3}>
-              
-              <MovieCard movie={movie} onToggleFavorite={handleToggleFavorite} />
-            </Col>
-          ))}
-        </Row>
-      )
-    }
-  />
-</Routes>
+          <Route
+            path="/login"
+            element={
+              user ? (
+                <Navigate to="/" replace />
+              ) : (
+                <LoginView onLoggedIn={(user, token) => {
+                  setUser(user);
+                  setToken(token);
+                  
+                  localStorage.setItem("token", token);
+                }} />
+              )
+            }
+          />
+          <Route
+            path="/signup"
+            element={<SignupView />}
+          />
+          <Route
+            path="/logout"
+            element={<Navigate to="/" replace />}
+          />
+          <Route
+            path="/profile"
+            element={<ProfileView movies={movies} onUpdateUser={handleUpdateUser} onDeregister={handleDeregister} onToggleFavorite={handleToggleFavorite} />}
+          />
+          <Route
+            path="/movies/:movieId"
+            element={<MovieView movies={movies} onToggleFavorite={handleToggleFavorite} />}
+          />
+          <Route
+            path="/"
+            element={
+              movies.length === 0 ? (
+                <Row>
+                  <Col>
+                    <div>The list is empty!</div>
+                  </Col>
+                </Row>
+              ) : (
+                <Row>
+                  {movies.map((movie) => (
+                    <Col key={movie.id} xs={12} sm={6} md={4} lg={3}>
+                      <MovieCard movie={movie} onToggleFavorite={handleToggleFavorite} />
+                    </Col>
+                  ))}
+                </Row>
+              )
+            }
+          />
+        </Routes>
       </Container>
     </Router>
   );
